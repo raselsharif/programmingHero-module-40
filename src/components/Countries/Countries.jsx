22f9791cfee08b59fa3db5bc1e87c4f4e3479233
Rasel Sharif <raselsharif9@gmail.com>
2023-09-09 20:58:38 +0600

    added load api

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+
+export default function Countries() {
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => console.log(data))
+}, [])
+return (
+  <div>Countries: </div>
+)
+}
