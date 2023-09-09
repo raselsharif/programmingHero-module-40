48b7931b79a626cf665f9ad83746aba8915e5828
Rasel Sharif <raselsharif9@gmail.com>
2023-09-09 22:07:14 +0600

    added visited handler

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,22 @@
 
+import { useState } from 'react';
 import './Country.css'
 function Country({ country }) {
-    const {name, flags, population, area} = country;
+    const { name, flags, population, area, cca2 } = country;
+
+    const [visited, setVisited] = useState(false)
+    const handleVisited = ()=>{
+        setVisited(!visited)
+    }
     return (
         <div className="country">
             <h3>Country Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Populations: {population}</p>
             <p>Area: {area}</p>
+            <p>Code: {cca2}</p>
+            <button onClick={handleVisited}>{visited ?  'visited':'want to visit'}</button>
+            
         </div>
     );
 }
